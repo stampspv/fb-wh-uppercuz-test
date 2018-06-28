2ad33e8a147866bf13107991793ddb03bbac304a
stampspv <suphavich.c@gmail.com>
2018-06-28 11:01:23 +0700

    1.2

--- verify.php
+++ verify.php
@@ -1,16 +1,5 @@
 <?php
 
-// require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
-
-// $fb = new \Facebook\Facebook([
-//   'app_id' => '{app-id}',
-//   'app_secret' => '{app-secret}',
-//   'default_graph_version' => 'v2.10',
-//   //'default_access_token' => '{access-token}', // optional
-// ]);
-
-echo "version 1.1";
-
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
@@ -20,7 +9,3 @@ if ($verify_token === 'stamplnwza555') {
 
 $input = json_decode(file_get_contents('php://input'), true);
 error_log(print_r($input, true));
-
-
-
- ?>
