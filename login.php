2ad95607140ea88b85a727e4c12cc107ef0d66cf
stampspv <suphavich.c@gmail.com>
2018-06-28 11:13:25 +0700

    1.4

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
 
 session_start();
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}', // Replace {app-id} with your app id
-  'app_secret' => '{app-secret}',
+  'app_id' => '1651280148319071', // Replace {app-id} with your app id
+  'app_secret' => 'ef76a8fe234fbc0ef4005cb889ae5d19',
   'default_graph_version' => 'v2.2',
   ]);
 
